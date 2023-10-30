b628f3c3447c666fcc4b0e29ad8b58cc4deeceaa
Valeria <Lerok.vasil007@yandex.ru>
2023-10-30 15:05:14 +0500

    test

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const httpProxy = require("http-proxy")
 // Create your proxy server and set the target in the options.
 //
 console.log(`process.env.TARGET_PORT`, process.env.TARGET_PORT)
+
 console.log(`Proxy listens on port`, process.env.PROXY_PORT)
 // httpProxy.createProxyServer({target:`http://172.30.138.30:${process.env.TARGET_PORT}`}).listen(process.env.PROXY_PORT || 9001); // See (†)
 
