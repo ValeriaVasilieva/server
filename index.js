87fd3c5db29f753f8e7445445844498903fa167f
Valeria <lerok007@doubletapp.io>
2023-10-30 15:05:03 +0500

    test

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const httpProxy = require("http-proxy")
 console.log(`process.env.TARGET_PORT`, process.env.TARGET_PORT)
 console.log(`Proxy listens on port`, process.env.PROXY_PORT)
 // httpProxy.createProxyServer({target:`http://172.30.138.30:${process.env.TARGET_PORT}`}).listen(process.env.PROXY_PORT || 9001); // See (†)
+
 httpProxy
   .createProxyServer({ target: `http://localhost:${process.env.TARGET_PORT}` })
   .listen(process.env.PROXY_PORT || 9001) // See (†)
